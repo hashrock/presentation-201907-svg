ffca2a0d011320a5dff42b1286428ca5722cdfe2
hashrock <hashrock@users.noreply.github.com>
2019-07-22 01:15:41 +0900

    add gradient editor

--- with-gsap/App.vue
+++ with-gsap/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div>
-    <div>
-      <input type="range" v-model="x1" />
-      <input type="range" v-model="y1" />
-      <input type="range" v-model="x2" />
-      <input type="range" v-model="y2" />
-    </div>
     <svg ref="canv" width="400" height="400" viewBox="0 0 400 400">
       <!-- objectは％単位 -->
       <defs>
         <linearGradient gradientUnits="userSpaceOnUse" id="g0" :x1="x1" :y1="y1" :x2="x2" :y2="y2">
           <stop offset="0" stop-color="black" />
-          <stop offset="1" stop-color="white" />
+          <stop offset="1" stop-color="yellowgreen" />
         </linearGradient>
       </defs>
-      <text x="100" y="50" fill="black">{{msg}}</text>
       <rect x="0" y="0" width="100" height="100" fill="url(#g0)" />
       <rect x="50" y="50" width="100" height="100" fill="url(#g0)" />
       <rect ref="p1" x="0" y="0" width="10" height="10" fill="white" stroke="black" />
+      <rect ref="p2" x="0" y="0" width="10" height="10" fill="white" stroke="black" />
     </svg>
   </div>
 </template>
+<style>
+body {
+  margin: 3rem;
+}
+</style>
 <script>
 import Draggable from "gsap/Draggable";
+import TweenLite from "gsap/TweenLite";
+
 export default {
   data() {
     return {
-      msg: "123",
       x1: 0,
       y1: 0,
       x2: 100,
@@ -34,11 +34,36 @@ export default {
     };
   },
   mounted() {
+    const self = this;
+    TweenLite.set(this.$refs.p1, {
+      x: this.x1,
+      y: this.y1
+    });
+    TweenLite.set(this.$refs.p2, {
+      x: this.x2,
+      y: this.y2
+    });
+
     Draggable.create(this.$refs.p1, {
       type: "x,y",
       edgeResistance: 0.65,
       bounds: this.$refs.canv,
-      throwProps: true
+      throwProps: true,
+      onDrag: function() {
+        self.x1 = this.x;
+        self.y1 = this.y;
+      }
+    });
+
+    Draggable.create(this.$refs.p2, {
+      type: "x,y",
+      edgeResistance: 0.65,
+      bounds: this.$refs.canv,
+      throwProps: true,
+      onDrag: function() {
+        self.x2 = this.x;
+        self.y2 = this.y;
+      }
     });
   }
 };
