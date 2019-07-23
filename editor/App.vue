cfc55121615bb629545f2d938305beb68bffd698
hashrock <hashrock@users.noreply.github.com>
2019-07-23 22:41:19 +0900

    add filter

--- editor/App.vue
+++ editor/App.vue
@@ -1,40 +1,56 @@
 <template>
-  <div contenteditable>
-    <svg width="300" height="300" ref="canv" viewBox="0 0 300 300">
-      <path :d="os" id="path1" />
-      <text class="output-text">
-        <textPath font-weight="900" font-size="30" href="#path1">TESTTESTTEST</textPath>
-      </text>
-      <g v-for="(item, idx) in o" :key="idx">
-        <line
-          :x1="item.p[0].x"
-          :y1="item.p[0].y"
-          :x2="item.p[1].x"
-          :y2="item.p[1].y"
-          stroke="black"
-        />
-        <line
-          :x1="item.p[2].x"
-          :y1="item.p[2].y"
-          :x2="item.p[3].x"
-          :y2="item.p[3].y"
-          stroke="black"
-        />
-      </g>
-      <g v-for="(item, idx) in o" :key="idx">
-        <g
-          @pointerdown="onPointerDown($event, point)"
-          @pointermove="onPointerMove"
-          @pointerup="onPointerUp"
-          v-for="(point, pidx) in item.p"
-          :key="pidx"
-          :transform="translate(point.x,point.y)"
-        >
-          <circle class="handle" r="10" x="0" y="0" fill="rgba(0,255,0,0.0)" />
-          <circle style="pointer-events: none;" r="5" x="0" y="0" fill="rgba(0,0,0,0.5)" />
+  <div>
+    <div contenteditable>
+      <svg width="300" height="300" ref="canv" viewBox="0 0 300 300">
+        <defs>
+          <filter id='noise' x='0%' y='0%' width='100%' height='100%'>
+            <feTurbulence type="turbulence" baseFrequency="0.01 0.1" numOctaves="1" result="NOISE" />
+            <feDisplacementMap in="SourceGraphic" in2="NOISE" scale="20" xChannelSelector="R" yChannelSelector="R"></feDisplacementMap>
+          </filter>
+        </defs>
+        <path :d="os" id="path1" />
+        <text class="output-text" :filter="svgFilter">
+          <textPath font-weight="900" font-size="30" href="#path1">TESTTESTTEST</textPath>
+        </text>
+        <g v-for="(item, idx) in o" :key="idx">
+          <line
+            :x1="item.p[0].x"
+            :y1="item.p[0].y"
+            :x2="item.p[1].x"
+            :y2="item.p[1].y"
+            stroke="black"
+          />
+          <line
+            :x1="item.p[2].x"
+            :y1="item.p[2].y"
+            :x2="item.p[3].x"
+            :y2="item.p[3].y"
+            stroke="black"
+          />
         </g>
-      </g>
-    </svg>
+        <g v-for="(item, idx) in o" :key="idx">
+          <g
+            @pointerdown="onPointerDown($event, point)"
+            @pointermove="onPointerMove"
+            @pointerup="onPointerUp"
+            v-for="(point, pidx) in item.p"
+            :key="pidx"
+            :transform="translate(point.x,point.y)"
+          >
+            <circle class="handle" r="10" x="0" y="0" fill="rgba(0,255,0,0.0)" />
+            <circle style="pointer-events: none;" r="5" x="0" y="0" fill="rgba(0,0,0,0.5)" />
+          </g>
+        </g>
+      </svg>
+    </div>
+    <color></color>
+    <div>
+      <label>
+        <input type="checkbox" v-model="enableFilter">
+        Apply Filter 
+      </label>
+
+    </div>
   </div>
 </template>
 
@@ -45,21 +61,26 @@ function screenToSvg(point, el, svg) {
   pt.y = point.y;
   return pt.matrixTransform(el.getScreenCTM().inverse());
 }
+import Color from "./Color.vue";
 
 export default {
+  components: {
+    Color
+  },
   props: {
     msg: String
   },
   data() {
     return {
+      enableFilter: false,
       o: [
         {
           type: "C",
           p: [
-            { x: 200, y: 200},
-            { x: 240, y: 200},
-            { x: 210, y: 250},
-            { x: 250, y: 250}
+            { x: 200, y: 200 },
+            { x: 240, y: 200 },
+            { x: 210, y: 250 },
+            { x: 250, y: 250 }
           ]
         }
       ],
@@ -107,6 +128,9 @@ export default {
           }
         })
         .join(" ");
+    },
+    svgFilter(){
+      return this.enableFilter ? `url(#noise)` : null
     }
   }
 };
@@ -119,11 +143,11 @@ path {
   stroke-width: 3;
 }
 
-.output-text{
+.output-text {
   font-size: 30rem;
 }
 
-.handle{
- cursor: pointer;
+.handle {
+  cursor: pointer;
 }
 </style>
